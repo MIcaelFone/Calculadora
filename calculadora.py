332cbfae37bf8e5835fe2d9a0c54c7f21c85b6d5
MicaelFone <micaelfone0@gmail.com>
2024-02-22 17:48:39 -0300

    adicioando a função de resultado e refatorando o calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,41 +1,38 @@
 import re
+
+#Perguntar o nome do arquivo
 arquivo = str (input("Digite o nome do arquivo: "))
-with open(arquivo+".txt", "r") as arquivo:
-    #ler o arquivo
-    calculos = arquivo.readlines()
 
-   # separar os valores do parenteses
-    
-    for linha in calculos:
+def resultado(linha_comandoresultado,calculos):
+                
+    #Verificando digitos
+    if linha_comandoresultado.isdigit():  # Check if the string is a digit
 
-        #remover os parenteses
-        expressao_parenteses=linha.replace("(","").replace(")","")
-           
-         # pegar valores do expressao 
-        valores = re.findall(r'\d+', linha)
+        #convertando a linha de str para int
 
-        #convertando os valores
-        int_list = [int(num) for num in valores]
+        line = int(linha_comandoresultado)
 
+        #pegar valores do expressao
+        valores = re.findall(r'\d+',calculos[line-1])
 
+        #convertando os valores de str para int 
+
+        int_linha_comandoresultado = [int(num) for num in valores]
+
+        operador_matematico = re.findall(r'[+\-*/|%^]', calculos[line-1])        
         
-        #pegar operador 
-
-        operador_matematico = re.findall(r'[+\-*/|%^]', linha)
-        
-           
         #realizar a operação de soma  
         if (operador_matematico[0] == "+"):
-            
-            resultado = int_list[0] + int_list[1]
+ 
+            resultado = int_linha_comandoresultado[0] + int_linha_comandoresultado[1]
             
             print(resultado)
 
-        #realiza a operação de subtração    
 
+        #realiza a operação de subtração    
         elif (operador_matematico[0] == "-"):
 
-            resultado = int_list[0] - int_list[1]
+            resultado = int_linha_comandoresultado[0] - int_linha_comandoresultado[1]
 
             print(resultado)    
 
@@ -43,7 +40,7 @@ with open(arquivo+".txt", "r") as arquivo:
             
         elif (operador_matematico[0] == "*"):
 
-            resultado = int_list[0] * int_list[1]
+            resultado = int_linha_comandoresultado[0] * int_linha_comandoresultado[1]
         
             print(resultado)        
         
@@ -51,7 +48,7 @@ with open(arquivo+".txt", "r") as arquivo:
         
         elif (operador_matematico[0] == "|"):
 
-            resultado = int_list[0] / int_list[1]
+            resultado = int_linha_comandoresultado[0] / int_linha_comandoresultado[1]
 
             print(resultado)
 
@@ -59,41 +56,92 @@ with open(arquivo+".txt", "r") as arquivo:
             
         elif (operador_matematico[0] == "/"):
            
-           resultado = int_list[0] // int_list[1]
+           resultado = int_linha_comandoresultado[0] // int_linha_comandoresultado[1]
            
            print(resultado)   
 
         
         elif (operador_matematico[0] == "%"):
 
-            resultado = int_list[0] % int_list[1]
+            resultado = int_linha_comandoresultado[0] % int_linha_comandoresultado[1]
 
             print(resultado)
 
         elif (operador_matematico[0] == "^"):
 
-            resultado = int_list[0] ** int_list[1]
+            resultado = int_linha_comandoresultado[0] ** int_linha_comandoresultado[1]
 
             print(resultado)   
 
-#Criar uma funçao para ler os três comandos especiais (N RES), (V MEM) e (MEM)
-        #1.N res traz o valor do resultado da operação
-            
-            
-           
-       
-        
-        
 
-            
-        
-             
-            
+def memoria(linha_comando_de_memoria,calculos):
+                
+    #Verificando digitos
+    if linha_comando_de_memoria.isdigit():  # Check if the string is a digit
+
+        #convertando a linha de str para int
+
+        line = int(linha_comando_de_memoria)
+
+        #pegar valores do expressao
+        valores = re.findall(r'\d+',calculos[line-1])
+
+        #convertando os valores de str para int 
+
+        int_linha_comando_memoria = [int(num) for num in valores]
 
 
-        #separar os valores e operadores
-        
-        
+
+def calculadora(arquivo):
+    
+    with open(arquivo+".txt", "r") as arquivo:
+
+        #ler o arquivo
+        calculos = arquivo.readlines()
+
+        # separar os valores do parenteses
+        for linha in calculos:
+
+            #remover os parenteses
+
+            linha.replace("(","").replace(")","")
+                
+            #pegar a lista do comando do resultado
+            comandoresultado = re.findall(r'\d+ RES', linha)
+
+            comandoresultado = ", ".join(comandoresultado)
+
+            comandoresultado = comandoresultado.replace(" ", ",")
+
+            #linha do comando do resultado
+            linha_comandoresultado = comandoresultado[0:1].replace(" ","")
+ 
+            #pegar o nome do comando do resultado
+            nome_do_comando_de_resultado= comandoresultado[2:]
+
+            #pegar comando da memoria
+
+            comando_de_memoria = re.findall(r'MEM', linha)
+
+            comando_de_memoria = ", ".join(comando_de_memoria)
+            
+
+            comando_de_memoria = comando_de_memoria.replace(" ", ",")
+
+            #linha do comando do resultado
+            linha_comando_de_memoria = comando_de_memoria[0:1].replace(" ","")
+ 
+            #pegar o nome do comando do resultado
+            nome_do_comando_de_memoria= comando_de_memoria[2:]
+
+
+            if(nome_do_comando_de_resultado):
+                resultado(linha_comandoresultado,calculos)
+
+            elif(nome_do_comando_de_memoria):
+                memoria(linha_comando_de_memoria,calculos)
+
+calculadora(arquivo)                               
 
         
        
